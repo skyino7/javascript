5e52135e9f298d58d84c13bcc74f40fca6fe9642
Seth Ketsi <christambassador12@gmail.com>
2023-12-04 15:23:46 +0000

    updated

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # JavaScript
+
+## Variable Declaration in JavaScript
+
+### var
+` var statement is used to declare variables in JavaScript that are function-scoped.`
+
+### let
+`let keyword is also used for variable declaration. But the only difference is the variable declared with the “let” keyword is block-scoped and local variable.`
+
+### const
+`The const declaration creates an immutable reference to a value. It does not mean the value it holds is immutable — just that the variable identifier cannot be reassigned.`
--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-alert("Welcome To Javascript");
+// alert("Welcome To Javascript");
 
-document.write("Welcome To Javascript");
+// document.write("Welcome To Javascript");
 
-console.log("Welcome To Javascript");
+// console.log("Welcome To Javascript");
+
+var x = 10;
+var fname = "Seth";
+
+document.write("My name is " + fname + ", I am " + x + " years old.");
+console.log(fname + " " + x)
+
+// alert(x);
+
+arr = ['Hello', 'Test', 'School']
+for (i = 0; i < arr.length; i++)
+    console.log(arr[i])
+
+
+let z = 10;
+
+if (z > 5)
+{
+    let y = 20;
+    console.log(y);
+}
+
+console.log(y);
